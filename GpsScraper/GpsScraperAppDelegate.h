c07589bd62ada60315229b1e8c2938ebd4a3b727
Mark Grandi <markgrandi@gmail.com>
2011-11-05 14:21:23 -0700

    renamed the files and added an empty nib file for the gps scraper

---
+++ GpsScraper/GpsScraperAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GpsScraper
+//
+//  Created by Mark Grandi on 11/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GpsScraperAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GpsScraper/GpsScraperViewController.h
@@ -0,0 +1,13 @@
+//
+//  GpsScraperViewController.h
+//  GpsScraper
+//
+//  Created by Mark Grandi on 11/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GpsScraperViewController : UIViewController
+
+@end
